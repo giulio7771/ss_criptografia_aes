8242681043a8e379058f04ffdb7babba9528ee1c
matheus mahnke <matheusmahnke@gmail.com>
2019-10-23 20:07:04 -0300

    'aula'

--- aes.py
+++ aes.py
@@ -1,3 +1,33 @@
+
+def rotacionar_palavra(palavra):
+    retorno = []
+    primeiro = palavra[0]
+    for i in range(1,4):
+        retorno[i - 1] = retorno [i]
+    retorno[3] = primeiro
+    return retorno
+
+def generateFirstColunmRoundKey(round_key_nova, round_key_anterior):
+    ultima_palavra = round_key_anterior[:][3]
+    rotacionar_palavra(ultima_palavra)
+
+def generateRoundKey(round_key_anterior):
+    round_key_nova = [[0 for x in range(4)] for y in range(4)]
+    round_key_nova = generateFirstColunmRoundKey(round_key_nova, round_key_anterior)
+    for i in range(0,4):
+        for j in range(1,4):
+            round_key_nova[i][j] = round_key_anterior[i][j-1] ^ round_key_nova[i][j-1]
+    return round_key_nova
+
+def expandirChave(round_key_original):
+    chave_mapa = []
+    chave_mapa.append(round_key_original)
+    round_key_atual = round_key_original
+    for i in range(10):
+        round_key_atual = generateRoundKey(round_key_atual)
+        chave_mapa.append(round_key_atual)
+    return chave_mapa
+
 def app():
     #arq_origem_nome = input("Informe o arquivo a ser cifrado\n")
     #chave = input("Informe os bytes que compoem a chave de 128 bits. Formato -> '20,1,94,...'\n")
@@ -12,7 +42,6 @@ def app():
             print(chaves[idx])
             chaves_mapa[j][i] = chaves[idx].encode()
             idx += 1
-    for i in chaves_mapa:
-        print(type(i[0]))
+    expandirChave(chaves_mapa)
 
 app()
